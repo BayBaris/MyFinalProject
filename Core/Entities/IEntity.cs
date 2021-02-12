7f436b9094fe1a899276f34e93200a116fcbbdd5
Barış KARAKAYA <181805054@ofis365.adu.edu.tr>
2021-02-12 17:55:01 +0300

    Core Katmanı Yazıldı

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    //Core Entities,tüm projelere hizmet edebilen bir interface'tir...
+    //IEntity implement eden bir class bir veri tabanı tablosudur...
+    public interface IEntity
+    {
+
+    }
+}
